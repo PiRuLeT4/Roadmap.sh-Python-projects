58f319fc17ddacc83b915e5094d2a5fad4a6db04
PiRuLeT4 <stiradoanula@gmail.com>
2025-07-05 23:40:41 +0200

    First version of github user activity

---
+++ BEGINNER/GitHub User Activity/main.py
@@ -0,0 +1,39 @@
+import requests
+import sys
+
+
+
+def main():
+
+    if len(sys.argv) != 2:
+        print("Error in username\n. Try: python main.py <username>.")
+        return
+    else:
+        username = sys.argv[1]
+
+
+    url = f"https://api.github.com/users/{username}/events"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+ 
+    for event in data:
+        type = event["type"]
+        repo_name = event["repo"]["name"]
+
+        if type == "PushEvent":
+            num_commits = len(event["payload"]["commits"])
+            print(f"Pushed {num_commits} commit{"s" if num_commits != 1 else ""} to {repo_name}")
+
+        elif type == "WatchEvent":
+            print(f"Starred {repo_name}")
+        elif type == "CreateEvent":
+            if event["payload"]["ref_type"] == "repository":
+                print(f"Created a new repository: {repo_name}")
+        elif type == "ForkEvent":
+            print(f"Forked {repo_name}")
+
+if __name__ == "__main__":
+    main()
